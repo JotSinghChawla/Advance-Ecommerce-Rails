cac322dd107f6c3451d382f4694631b156ced576
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-21 16:36:11 +0530

    Added authenticate_admin function in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+    protected
+
+    def authenticate_admin!
+        authenticate_user!
+        flash[:alert] = "You are not authorized to access this page." unless current_user.admin?
+        redirect_to '/' unless current_user.admin?
+    end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[ show edit update destroy ]
 
+  before_action :authenticate_admin!, only: [:new, :create, :edit, :update, :destroy]
+
   # GET /items or /items.json
   def index
     @items = Item.all
