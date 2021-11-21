c20b56b5e59fdcdd3f048c4ef88f418bb2233dbd
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-21 18:02:37 +0530

    Added 'Cart' controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'carts/show'
   resources :items
   
   devise_for :users
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :order_items
+
+  resource :carts, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
