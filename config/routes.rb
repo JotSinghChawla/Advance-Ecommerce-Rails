e8c49582155600a58d88ab511180e7a4bab7b1af
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-21 17:17:44 +0530

    Added 'add to cart' feature in view file & Created 'OrderItems' controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :items
+  
   devise_for :users
+  
   root 'home#index'
+
+  resources :order_items
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
