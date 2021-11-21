8b8e863bf761c762c6c04249b9a60c33cf6b3a0e
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-22 05:02:45 +0530

    Added Update & Destroy feature in 'Cart' View

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,12 +1,29 @@
 class OrderItemsController < ApplicationController
 
-
     def create
         @order = current_order
         @order_item = @order.order_items.new(oi_params)
         @order.save
         session[:order_id] = @order.id
     end
+
+
+    def update
+        @order = current_order
+        @order_item = @order.order_items.find(params[:id])
+        @order_item.update_attributes(oi_params)
+        @order_items = @order.order_items
+    end
+
+    
+    def destroy
+        @order = current_order
+        @order_item = @order.order_items.find(params[:id])
+        @order_item.destroy
+        @order_items = @order.order_items
+    end
+
+
 
     private 
 
